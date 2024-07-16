52438b3c56f128733316e5d33cd2b5d2a05d3340
Ky <kylesmith97@outlook.com>
2024-07-16 15:59:31 +0100

    components readied

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import { useState } from 'react'
+import Footer from './components/footer.jsx';
+import Header from './components/header.jsx';
+import Hero from './components/hero.jsx';
 
 
 function App() {
   
   return (
-    <></>)
+    <div>
+      <Header />
+    </div>
+   )
 }
 
 export default App
