d4504d7c3ffad2f68ca48299ca23087dbbf15047
Ky <kylesmith97@outlook.com>
2024-07-17 14:47:07 +0100

    hero section 1

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 
 
+
 const Hero = () => {
   return (
     <>
     <Section1 />
+    <Section2 />
     </>
+    
+    
   )
 }
 
 const Section1 = () => {
   return (
     <div className="sectionContainer1">
-      <h1>Live 12.1 is now in public beta</h1>
+      <div className="section1textbox">
+      <h1 className="h1Hero">Live 12.1 is now in public beta</h1>
+      <p className="section1p">See what's new &gt;</p>
+      </div>
+      
     </div>
+  )
+}
+
+const Section2 = () => {
+  return (
+    <>
+    <div className="sectionContainer2">
+      
+      
+    </div>
+    </>
   )
 }
 
